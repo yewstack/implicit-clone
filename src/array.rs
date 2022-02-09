3106d8bf964b893cc9c1423b43e59db56e1619c9
Cecile Tonglet <cecile.tonglet@cecton.com>
2022-02-09 12:17:11 +0100

    IArray: add into_iter(), get() and as_slice()

--- src/array.rs
+++ src/array.rs
@@ -75,4 +75,42 @@ impl<T: 'static> IArray<T> {
             Self::Rc(a) => a.len(),
         }
     }
+
+    pub fn as_slice(&self) -> &[T] {
+        match self {
+            Self::Static(a) => &a,
+            Self::Rc(a) => &a,
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        match self {
+            Self::Static(a) => a.get(index),
+            Self::Rc(a) => a.get(index),
+        }
+    }
+}
+
+impl<T: Clone + 'static> IArray<T> {
+    pub fn into_iter(self) -> IArrayIntoIter<T> {
+        IArrayIntoIter {
+            array: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct IArrayIntoIter<T: Clone + 'static> {
+    array: IArray<T>,
+    index: usize,
+}
+
+impl<T: Clone + 'static> Iterator for IArrayIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.array.get(self.index).map(|x| x.clone());
+        self.index += 1;
+        item
+    }
 }
