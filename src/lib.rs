40a5ec874a4db4585b40976e3ae461b8434ec0c5
Alex Parrill <alex.parrill@col32.net>
2023-10-24 14:45:45 -0400

    Use mod paths instead of include (#33)
    This approach still allows using the same files for Rc and Arc while
    preventing imports from leaking as well as working better with
    rust-analyzer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(missing_debug_implementations, missing_docs, unreachable_pub)]
 #![allow(clippy::unnecessary_lazy_evaluations)]
+#![allow(clippy::duplicate_mod)]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
--- src/sync.rs
+++ src/sync.rs
@@ -1,11 +1,18 @@
+use std::sync::Arc as Rc;
+
 use crate::ImplicitClone;
-use std::fmt;
 
-type Rc<T> = std::sync::Arc<T>;
-
-include!("string.rs");
-include!("array.rs");
+#[path = "array.rs"]
+mod array;
 #[cfg(feature = "map")]
-include!("map.rs");
+#[path = "map.rs"]
+mod map;
+#[path = "string.rs"]
+mod string;
+
+pub use array::IArray;
+#[cfg(feature = "map")]
+pub use map::IMap;
+pub use string::IString;
 
 impl<T: ?Sized> ImplicitClone for Rc<T> {}
